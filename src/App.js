67496d33708667cd032e7a6d11c3af015ec3c585
rich-alien <Richalien@yandex.ru>
2021-01-30 18:32:38 +0300

    import + render components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/header/header";
+import Banner from "./components/baner/banner";
+import ProductBanner from "./components/product-banner/product-banner";
+import ProductSlider from "./components/product-slider/product-slider";
+import Footer from "./components/footer/footer";
+import SelectYear from "./components/select-year/select-year";
 
 
 const App = () => {
-  return (
-      <div className="App">
-        <Header/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="header">
+                <Header/>
+                <Banner/>
+            </div>
+            <div className="main-container">
+                <ProductBanner/>
+                <ProductSlider/>
+            </div>
+            <SelectYear/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
