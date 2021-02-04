b92a97ec783220bb61e1c279e137299952bc73fc
rich-alien <Richalien@yandex.ru>
2021-02-04 04:18:37 +0300

    Routing by page is set up. Without redirect and page not founding, because bag!

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Store from "./pages/store/store";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import {BrowserRouter, Route} from "react-router-dom";
+import News from "./pages/news/news";
 
 const App = () => {
     return (
         <BrowserRouter>
             <div className="App">
                 <Header/>
-                <Route path='/store' render={() => <Store/>}/>
-                <Route path='/' render={() => <Home/>}/>
+                    <Route path='/store' render={() => <Store/>}/>
+                    <Route path='/home' render={() => <Home/>}/>
+                    <Route path='/news' render={() => <News/>}/>
                 <Footer/>
             </div>
         </BrowserRouter>
