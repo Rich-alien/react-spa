e20dad84cb99d8164bf555e5be4a483096448c49
rich-alien <Richalien@yandex.ru>
2021-02-04 04:43:18 +0300

    fix style and import component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,22 @@
 import * as React from "react";
-// import Banner from "./components/baner/banner";
-// import ProductBanner from "./components/product-banner/product-banner";
-// import ProductSlider from "./components/product-slider/product-slider";
-// import SelectYear from "./components/select-year/select-year";
-class Home extends React.Component{
-    render() {
-        return(
-            <div>
+import ProductBanner from "../.././components/product-banner/product-banner";
+import ProductSlider from "../.././components/product-slider/product-slider";
+import SelectYear from "../.././components/select-year/select-year";
+import style from "./home.module.css"
 
+class Home extends React.Component {
+    render() {
+        return (
+            <div className={style['home-container']}>
+                {/*<Banner/> под редизайн!*/}
+                <div className={style['home-viewer']}>
+                    <ProductBanner/>
+                    <ProductSlider/>
+                </div>
+                <SelectYear/>
             </div>
         )
     }
 }
+
 export default Home;
