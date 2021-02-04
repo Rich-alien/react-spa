4f9d4dec3811af371e4b547257ae3ab646281961
rich-alien <Richalien@yandex.ru>
2021-02-04 04:42:19 +0300

    commented because unused and brings bags

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,14 +1,14 @@
-import * as React from "react";
-
-class NotFound extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Hi</p>
-                <button>sdgfsdfsd</button>
-            </div>
-        )
-    }
-}
-
-export default NotFound;
+// import * as React from "react";
+//
+// class NotFound extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <p>Hi</p>
+//                 <button>sdgfsdfsd</button>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default NotFound;
