4caa674ae05b2ed8773e55c706c2622390a6dc55
rich-alien <Richalien@yandex.ru>
2021-02-04 03:46:42 +0300

    Templates were created for the pages and styles for them.

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+// import Banner from "./components/baner/banner";
+// import ProductBanner from "./components/product-banner/product-banner";
+// import ProductSlider from "./components/product-slider/product-slider";
+// import SelectYear from "./components/select-year/select-year";
+class Home extends React.Component{
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Home;
---
+++ src/pages/store/store.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Store extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Hi</p>
+            </div>
+        )
+    }
+}
+
+export default Store;
