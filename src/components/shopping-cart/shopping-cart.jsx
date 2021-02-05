5cae0865f90ee46f642517ba2826182b83d7f46b
rich-alien <Richalien@yandex.ru>
2021-02-05 05:32:21 +0300

    moved to another folder , also added styles for the container

--- src/components/shopping-cart/shopping-cart.jsx
+++ src/components/shopping-cart/shopping-cart.jsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-
+import style from './shopping-cart.module.css';
 class ShoppingCart extends React.Component {
     render() {
         return (
-            <div>
-                <p>Hi</p>
+            <div className={style[this.props.hisActive?'container':'hide']}>
+                обычная корзина
             </div>
         )
     }
