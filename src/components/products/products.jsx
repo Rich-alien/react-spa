b816b1673942889552e11608161592bf89769f7e
rich-alien <Richalien@yandex.ru>
2021-02-05 03:20:19 +0300

    create function for hide and show button for adding product in cart. And was created minimal style

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -3,15 +3,36 @@ import style from "./products.module.css"
 import productTemplate from "../.././img/product.jpg";
 
 class Products extends React.Component {
+    state = {
+        onLeadProduct: false
+    }
+
     render() {
         return (
-            <div className={style['product-container']}>
+            <div onMouseOver={this.showButton} onMouseLeave={this.hideButton} className={style['product-container']}>
                 <img className={style['product-image']} src={productTemplate} alt="img product"/>
                 <div className={style['product-name']}>{this.props.name}</div>
                 <div className={style['product-price']}>{this.props.price}</div>
+                <button className={this.state.onLeadProduct ? style['product-button__show'] : style['product-button__hidden']}>
+                    добавить в корзину
+                </button>
             </div>
         )
     }
+
+    showButton = () => {
+        this.setState({
+            ...this.state,
+            onLeadProduct: true
+        })
+    }
+    hideButton = () =>{
+        this.setState({
+            ...this.state,
+            onLeadProduct: false
+        })
+    }
 }
+
 
 export default Products;
