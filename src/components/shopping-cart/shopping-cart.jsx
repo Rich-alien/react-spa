22509347f57bab32f8f6eee2f85fb33690b9b1c5
rich-alien <Richalien@yandex.ru>
2021-02-08 01:17:36 +0300

    take the data from the props and run through the function to get with the product id and check for collision. + style

--- src/components/shopping-cart/shopping-cart.jsx
+++ src/components/shopping-cart/shopping-cart.jsx
@@ -1,10 +1,66 @@
 import * as React from "react";
 import style from './shopping-cart.module.css';
+import {products} from "../../data/products.data"
+import CartLine from "../cart-line/cart-line";
+
+let cart = [];
+let cartCount = 0;
+
 class ShoppingCart extends React.Component {
+    state = {};
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.index !== null && this.props.index !== '') {
+            console.log(this.props.index)
+            this.renderProductInCart()
+        }
+    }
+
+    renderProductInCart = () => {
+        const {index} = this.props;
+        if (cartCount === 0) {
+            cartCount++;
+            this.addProductToCart(index);
+        } else {
+            if (this.hasCollision(index)) {
+                cartCount++;
+                this.addProductToCart(index);
+            } else {
+                cart.forEach(item => {
+                    if (item.id === index) {
+                        item.count++;
+                    }
+                });
+            }
+        }
+
+    }
+
+    addProductToCart(id) {
+        cart.push(products[id]);
+    }
+
+    hasCollision(id) {
+        let HasId = true;
+        cart.forEach(item => {
+            if (item.id === id) {
+                HasId = false;
+            }
+        });
+        return HasId;
+    }
+
     render() {
+
         return (
-            <div className={style[this.props.hisActive?'container':'hide']}>
-                обычная корзина
+            <div className={style[this.props.hisActive ? 'container' : 'hide']}>
+
+                {
+                    cart.map((item, index) => (<CartLine key={index} number={item.name}/>))
+                }
+                <button onClick={this.props.closeCart}>
+                    X
+                </button>
             </div>
         )
     }
