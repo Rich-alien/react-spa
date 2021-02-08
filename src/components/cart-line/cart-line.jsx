34de1948014a681d473437c570fda4304db9a3cf
rich-alien <Richalien@yandex.ru>
2021-02-08 23:52:29 +0300

    add function increment and  decrement

--- src/components/cart-line/cart-line.jsx
+++ src/components/cart-line/cart-line.jsx
@@ -1,16 +1,50 @@
 import * as React from "react";
 import style from './cart-line.module.css';
-import ProductCounter from "../product-counter/product-counter";
 
 class CartLine extends React.Component {
+    state = {
+        count: 0
+    }
+
+    componentDidMount() {
+        const {count} = this.props
+        this.setState({
+            ...this.state,
+            count,
+        })
+    }
+
     render() {
         return (
             <div className={style.container}>
                 {this.props.name}
-                {this.props.price * this.props.count}
-                <ProductCounter count={this.props.count}/>
+                {this.props.price * this.state.count}
+                <div>
+                    <div onClick={this.increment}>
+                        +
+                    </div>
+                    <div className={style.counter}>
+                        {this.state.count}
+                    </div>
+                    <div onClick={this.decrement}>
+                        -
+                    </div>
+                </div>
             </div>
         )
+    }
+
+    increment = () => {
+        this.setState({
+            ...this.state,
+            count: this.state.count + 1
+        })
+    }
+    decrement = () => {
+        this.setState({
+            ...this.state,
+            count: this.state.count - 1
+        })
     }
 }
 
