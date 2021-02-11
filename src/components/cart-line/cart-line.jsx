da8caf69894310841a090cf5f062576add373f71
rich-alien <Richalien@yandex.ru>
2021-02-12 02:58:50 +0300

    using usingEffect for imitation componentDidMount

--- src/components/cart-line/cart-line.jsx
+++ src/components/cart-line/cart-line.jsx
@@ -1,11 +1,13 @@
-import * as React from "react";
+import React, {useEffect, useState} from "react";
 import style from './cart-line.module.css';
 
 
 const FunctionalCartLine = ({name, price, count}) => {
 
-    const [value, setValue] = React.useState(count)
-
+    const [value, setValue] = useState(0)
+    useEffect(() => {
+        setValue(count)
+    },[])
     const increment = () => {
         setValue(value + 1);
     };
