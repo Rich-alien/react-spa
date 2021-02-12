052b709337918781a4fddbad9a6cdaeea6e468c7
rich-alien <Richalien@yandex.ru>
2021-02-12 03:43:07 +0300

    refactoring to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Home from "./pages/home/home";
 import Store from "./pages/store/store";
@@ -8,30 +8,18 @@ import {BrowserRouter, Route} from "react-router-dom";
 import News from "./pages/news/news";
 
 
-class App extends React.Component {
-    state = {
-        index: ''
-    }
-
-    render() {
-        return (
-            <BrowserRouter>
-                <div className="App">
-                    <Header index={this.state.index}/>
-                    <Route path='/store' render={() => <Store addProductInCart={this.addProductInCart}/>}/>
-                    <Route path='/home' render={() => <Home/>}/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Footer/>
-                </div>
-            </BrowserRouter>
-        )
-    }
-
-    addProductInCart = (id) => {
-        this.setState({
-            index: id
-        })
-    }
+const App = () => {
+    const [indexCart, setIndexCart] = useState(0);
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Header index={indexCart}/>
+                <Route path='/store' exact render={() => <Store addProductInCart={setIndexCart}/>}/>
+                <Route path='/home' exact render={() => <Home/>}/>
+                <Route path='/news' exact render={() => <News/>}/>
+                <Footer/>
+            </div>
+        </BrowserRouter>
+    )
 }
-
 export default App;
