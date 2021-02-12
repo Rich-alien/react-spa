b734f4d37a711e99abb401a4795fdd805c92575e
rich-alien <Richalien@yandex.ru>
2021-02-12 10:13:44 +0300

    dont worked

--- src/components/shopping-cart/shopping-cart.jsx
+++ src/components/shopping-cart/shopping-cart.jsx
@@ -1,29 +1,21 @@
-import * as React from "react";
+import React, {useState, useEffect,useMemo} from "react";
 import style from './shopping-cart.module.css';
-import {products} from "../../data/products.data"
-// import CartLine from "../cart-line/cart-line";
+import {productsData} from "../../data/products.data"
 import FunctionalCartLine from "../cart-line/cart-line";
 
-let cart = [];
-let cartCount = 0;
 
-class ShoppingCart extends React.Component {
-    state = {}
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.index !== null && this.props.index !== '') {
-            console.log(this.props.index)
-            this.renderProductInCart()
-        }
-    }
-    renderProductInCart = () => {
-        const {index} = this.props;
+let cartCount = 0;
+const useCart = () => {
+    // const [indexToCart, setIndexToCart] = useState(0);
+    const [cart, setCart] = useState([]);
+    const renderProductInCart = (index) => {
         if (cartCount === 0) {
             cartCount++;
-            this.addProductToCart(index);
+            addProductToCart(index);
         } else {
-            if (this.hasCollision(index)) {
+            if (hasCollision(index)) {
                 cartCount++;
-                this.addProductToCart(index);
+                addProductToCart(index);
             } else {
                 cart.forEach(item => {
                     if (item.id === index) {
@@ -32,12 +24,11 @@ class ShoppingCart extends React.Component {
                 });
             }
         }
-
     }
-    addProductToCart(id) {
-        cart.push(products[id]);
+    const addProductToCart = (id) => {
+        setCart(productsData[id]);
     }
-    hasCollision(id) {
+    const hasCollision = (id) => {
         let HasId = true;
         cart.forEach(item => {
             if (item.id === id) {
@@ -46,23 +37,37 @@ class ShoppingCart extends React.Component {
         });
         return HasId;
     }
-    render() {
-        return (
-            <div className={style[this.props.hisActive ? 'container' : 'hide']}>
-                {
-                    cart.map((item, index) => (<FunctionalCartLine
-                        key={index}
-                        name={item.name}
-                        price={item.price}
-                        count={item.count}
-                        />))
-                }
-                <button onClick={this.props.closeCart}>
-                    back
-                </button>
-            </div>
-        )
+    return {
+        cart,
+        setCart,
+        renderProductInCart
+
     }
+}
+const ShoppingCart = ({hisActive, closeCart, index}) => {
+    const {
+        cart,
+        renderProductInCart
+    } = useCart(index);
+    useEffect(() => {
+        renderProductInCart()
+    }, [])
+    return (
+        <div className={style[hisActive ? 'container' : 'hide']}>
+            {cart === undefined ?
+                <div>В корзине пока что нет товаров </div> :
+                cart.map((item, index) => (<FunctionalCartLine
+                    key={index}
+                    name={item.name}
+                    price={item.price}
+                    count={item.count}
+                />))
+            }
+            <button onClick={closeCart}>
+                back
+            </button>
+        </div>
+    )
 }
 
 export default ShoppingCart;
