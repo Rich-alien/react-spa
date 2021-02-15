5a73be9b4a3b3f535be2e7422e944a1bd7c4a2f8
rich-alien <Richalien@yandex.ru>
2021-02-15 03:54:33 +0300

    add switch teg , component for view details product, and component 404

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import Home from "./pages/home/home";
 import Store from "./pages/store/store";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch, useLocation} from "react-router-dom";
 import News from "./pages/news/news";
+import DescriptionProduct from "./components/description-product/description-product";
 
 
 const App = () => {
-    const [indexCart, setIndexCart] = useState(0);
+    const [indexCart, setIndexCart] = useState(null);
+
+    // const location = useLocation()
+    // const params = new URLSearchParams(location.search);
+    // const q=params.get("q")
+    // console.log(q);
+
     return (
-        <BrowserRouter>
             <div className="App">
                 <Header index={indexCart}/>
-                <Route path='/store' exact render={() => <Store addProductInCart={setIndexCart}/>}/>
-                <Route path='/home' exact render={() => <Home/>}/>
-                <Route path='/news' exact render={() => <News/>}/>
+
+                <Switch>
+                    <Route path='/store' exact render={() => <Store addProductInCart={setIndexCart}/>}/>
+                    <Route path='/' exact render={() => <Home/>}/>
+                    <Route path='/news' exact render={() => <News/>}/>
+                    <Route path="/products/:id">
+                        <DescriptionProduct/>
+                    </Route>
+                    <Route>
+                        <h1 style={{color: 'white'}}>Error</h1>
+                    </Route>
+                </Switch>
                 <Footer/>
             </div>
-        </BrowserRouter>
     )
 }
 export default App;
