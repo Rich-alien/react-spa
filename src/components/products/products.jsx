7e30f8c840f71f83f7d40394d422a52c028e01a8
rich-alien <Richalien@yandex.ru>
2021-02-15 03:55:48 +0300

    add Link  to the description page

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import style from "./products.module.css"
 import productTemplate from "../.././img/product.jpg";
+import {Link} from "react-router-dom";
 
 const Products = ({name, price, id, addProductInCart}) => {
     const [onLeadProduct, setOnLeadProduct] = useState(false);
@@ -30,6 +31,11 @@ const Products = ({name, price, id, addProductInCart}) => {
                         'product-button__hidden']}>
                 добавить в корзину
             </button>
+            <Link to={`/products/${id}`} className={style[onLeadProduct ?
+                        'product-button-description__show' :
+                        'product-button__hidden']}>
+               описание
+            </Link>
         </div>
     )
 }
