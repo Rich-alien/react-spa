1a2564b82230727a64a2a6ed8358caf1d019324f
rich-alien <Richalien@yandex.ru>
2021-02-16 02:59:46 +0300

    created form

--- src/components/log-in/log-in.jsx
+++ src/components/log-in/log-in.jsx
@@ -2,9 +2,17 @@ import React, {useState} from "react";
 const LogIn = () => {
     const [data,setData] = useState([]);
     return (
-        <div>
-            <input type="text"/>
-        </div>
+        <form className="form">
+            <div className="field">
+                <label>First name</label>
+                <input type="text" name="first-name" placeholder="First Name"/>
+            </div>
+            <div className="field">
+                <label>Last name</label>
+                <input type="text" name="last-name" placeholder="Last Name"/>
+            </div>
+            <button className="button" type="submit">Submit</button>
+        </form>
     )
 }
 export default LogIn
