08da02574dc34f9b6ba77a59debda20f15b3ad03
rich-alien <Richalien@yandex.ru>
2021-02-19 18:22:46 +0300

    add teg provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux'
 import App from './App';
+import store from './store'
 import {BrowserRouter} from "react-router-dom";
-const root = document.getElementById('root');
-const Root =()=>(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
 
+const root = document.getElementById('root');
+const Root = () => (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 )
-ReactDOM.render(<Root/>,root)
+ReactDOM.render(<Root/>, root)
