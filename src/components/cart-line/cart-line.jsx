b5e17043b71d4182ee1d2c0b73a27e69a150d2af
rich-alien <Richalien@yandex.ru>
2021-02-20 03:51:15 +0300

    add from reducer increase and decrease

--- src/components/cart-line/cart-line.jsx
+++ src/components/cart-line/cart-line.jsx
@@ -1,62 +1,44 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import style from './cart-line.module.css';
 import {useDispatch} from "react-redux";
 
-const useCounter = () => {
-    const [value, setValue] = useState(0)
+const FunctionalCartLine = ({name, index}) => {
 
-    const increment = () => {
-        setValue(value + 1);
-    };
-    const decrement = () => {
-        setValue(value - 1);
-    };
-
-    return {
-        value,
-        setValue,
-        increment,
-        decrement
-    }
-}
-
-const FunctionalCartLine = ({name, price, count,id}) => {
-    const {
-        value,
-        setValue,
-        increment,
-        decrement
-    } = useCounter()
-
-    useEffect(() => {
-        setValue(count)
-    }, [count, setValue])
     const dispatch = useDispatch()
-    const deleteProduct = (index) => {
+
+    const handleIncrement = (index) => {
         dispatch({
-            type: 'deleteFromCart',
+            type: 'incrementProduct',
             payload: {
-                id: index,
+                id: index
             }
         })
     }
-
+    const handleDecrement = (index) => {
+        dispatch({
+            type: 'decrementProduct',
+            payload: {
+                id: index
+            }
+        })
+    }
     return (
         <div className={style.container}>
             {name}
-            {price * value}
+            {/*{price * value}*/}
             <div>
-                <div onClick={increment}> +</div>
-                <div className={style.counter}>
-                    {value}
+                <div onClick={() => {
+                    handleIncrement(index)
+                }}> +
                 </div>
-                <div onClick={decrement}>-</div>
+                <div className={style.counter}>
+                    {/*{value}*/}
+                </div>
+                <div onClick={() => {
+                    handleDecrement(index)
+                }}>-
+                </div>
             </div>
-            {/*<button onClick={() => {*/}
-            {/*    deleteProduct(id)*/}
-            {/*}}>delete*/}
-            {/*</button>*/}
-
         </div>
     )
 }
