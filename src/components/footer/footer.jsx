97e0679e5ba6534bfefea23d9a6ef5c1a14524c5
rich-alien <Richalien@yandex.ru>
2021-02-20 03:51:28 +0300

    functional component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,16 +1,15 @@
 import * as React from "react";
 import style from "./footer.module.css"
-class Footer extends React.Component{
-    render() {
-        return(
-            <footer className={style.footer}>
-                <div className={style.top}>
-                    {/*<img className={style.top__img} src="#" alt="footer-img"/>*/}
-                </div>
-                <div className={style.bottom}>
-                </div>
-            </footer>
-        )
-    }
+
+const Footer = () => {
+    return (
+        <footer className={style.footer}>
+            <div className={style.top}>
+                {/*<img className={style.top__img} src="#" alt="footer-img"/>*/}
+            </div>
+            <div className={style.bottom}>
+            </div>
+        </footer>
+    )
 }
 export default Footer;
