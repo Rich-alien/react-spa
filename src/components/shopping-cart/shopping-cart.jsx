3d1760a07eb1633c741adae57959a1cf88ad1f8c
rich-alien <Richalien@yandex.ru>
2021-02-22 00:08:03 +0300

    restored the block with the output of goods

--- src/components/shopping-cart/shopping-cart.jsx
+++ src/components/shopping-cart/shopping-cart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from './shopping-cart.module.css';
 import {useDispatch, useSelector} from "react-redux";
+import FunctionalCartLine from "../cart-line/cart-line";
 
 const ShoppingCart = () => {
     const products = useSelector(state => state.cart.products)
@@ -21,23 +22,16 @@ const ShoppingCart = () => {
     }
     return (
         <div className={style[hisActive ? 'container' : 'hide']}>
-            {
-                products.map(item=>item.name)
+            {products === undefined ?
+                <div>В корзине пока что нет товаров </div> :
+                products.map((item, index) => (<FunctionalCartLine
+                    key={index}
+                    id={item.id}
+                    name={item.name}
+                    price={item.price}
+                    count={item.count}
+                />))
             }
-            {
-                products.map(item=>item.count)
-            }
-            {/* на n время */}
-            {/*{products === undefined ?*/}
-            {/*    <div>В корзине пока что нет товаров </div> :*/}
-            {/*    products.map((item, index) => (<FunctionalCartLine*/}
-            {/*        key={index}*/}
-            {/*        id={index}*/}
-            {/*        name={item.name}*/}
-            {/*        price={item.price}*/}
-            {/*        count={item.count}*/}
-            {/*    />))*/}
-            {/*}*/}
 
             <button className={style.delete__all} onClick={handleDeleteCart}>delete all</button>
             <button className={style.close} onClick={handleCloseCart}>close</button>
